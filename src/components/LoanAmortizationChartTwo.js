80ab15e1d4b8297df4a306dcc580cc1d5ebe5c15
John Shellabarger <johnwilliamshellabarger@gmail.com>
2021-11-04 14:45:07 -0600

    test

--- src/components/LoanAmortizationChartTwo.js
+++ src/components/LoanAmortizationChartTwo.js
@@ -6,7 +6,7 @@ const LoanAmortizationChartTwo = ( { principlePaymentChartData } ) => {
 
   return (
     <div className='chart-container'>
-      <h1 className='cal-subheader-two'>Principle Payment Chart</h1>
+      <h1 className='cal-subheader-two'>Principle Payment Chart </h1>
       <Line 
         data={
           principlePaymentChartData
